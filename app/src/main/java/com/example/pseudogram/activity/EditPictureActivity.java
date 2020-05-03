8720a6429636924b5da71425669a52fc37f02e81
Leonardo Moraes <leonardo.dalla@hotmail.com>
2020-05-02 21:15:29 -0300

    Fix delete image file

--- app/src/main/java/com/example/pseudogram/activity/EditPictureActivity.java
+++ app/src/main/java/com/example/pseudogram/activity/EditPictureActivity.java
@@ -7,7 +7,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,9 +20,12 @@ import com.example.pseudogram.repository.PictureDao;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.UUID;
 
 public class EditPictureActivity extends AppCompatActivity {
+    // Value to request image capture
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     // View Variables
     private ImageView imageView;
@@ -28,27 +33,35 @@ public class EditPictureActivity extends AppCompatActivity {
     private TextView description;
     private Bitmap bitmap;
 
+    // buttons
+    private Button btnSave;
+    private Button btnDelete;
+    private Button btnTakePicture;
+
     // Object Variables
     private Picture currentPicture;
     private PictureDao pictureDao;
-
-    // Value to request image capture
-    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // View Variables
         setContentView(R.layout.activity_edit_picture);
+        pictureDao = new PictureDao(getApplicationContext());
+
         imageView = findViewById(R.id.imageView);
         title = findViewById(R.id.title);
         description = findViewById(R.id.description);
-        pictureDao = new PictureDao(getApplicationContext());
+
+        btnSave = findViewById(R.id.btnSave);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+
         // Initialize currentPicture variable
         currentPicture = (Picture) getIntent().getSerializableExtra("EXTRA_PICTURE");
         if (currentPicture == null || currentPicture.getId() == null) {
             this.currentPicture = new Picture();
-            findViewById(R.id.btnRemover).setEnabled(false);
+            btnDelete.setEnabled(false);
         } else {
             title.setText(currentPicture.getTitle());
             description.setText((currentPicture.getDescription()));
@@ -57,73 +70,8 @@ public class EditPictureActivity extends AppCompatActivity {
         }
     }
 
-    public void save(View view) {
-        // Get data from screen
-        currentPicture.setTitle(this.title.getText().toString());
-        currentPicture.setDescription(this.description.getText().toString());
-
-        try { // Save Picture data into Database
-            // If title or description are invalid, do not allow saving
-            if ((currentPicture.getTitle() == null || currentPicture.getTitle().equals("") ||
-                    currentPicture.getTitle().isEmpty()) ||
-                    (currentPicture.getDescription() == null || currentPicture.getDescription().equals("") ||
-                    currentPicture.getDescription().isEmpty())) {
-                Toast.makeText(getApplicationContext(), "Cannot Save without title or description",
-                        Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (currentPicture.getId() == null) { // New Picture
-                currentPicture.setPath(this.savePictureFile(this.bitmap, UUID.randomUUID()
-                        .toString() + ".jpg"));
-                pictureDao.insert(currentPicture);
-            } else { // Already Existent Picture
-                this.deletePictureFile(currentPicture.getPath()); // Deletes Previous Image
-                currentPicture.setPath(this.savePictureFile(this.bitmap, UUID.randomUUID()
-                        .toString() + ".jpg"));
-                pictureDao.update(currentPicture);
-            }
-            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            System.out.println(e);
-            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-        }
-        finish();
-    }
-
-    public void delete(View view) {
-        this.deletePictureFile(this.currentPicture.getPath());
-        pictureDao.delete(this.currentPicture);
-        finish();
-    }
-
-    private String savePictureFile(Bitmap imageToSave, String fileName) {
-        File file = new File(this.getApplicationContext().getFilesDir(), fileName);
-        if (file.exists()) {
-            file.delete();
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return file.getPath();
-    }
-
-    private void deletePictureFile(String filePath) {
-        File file = new File(this.getApplicationContext().getFilesDir(), filePath);
-        file.delete();
-    }
-
-    public void takePicture(View view) {
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+//    ------------------------------------------------------
+//    button actions
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -135,5 +83,78 @@ public class EditPictureActivity extends AppCompatActivity {
         }
     }
 
+
+    public void save(View view) {
+        // Get data from screen
+        currentPicture.setTitle(this.title.getText().toString());
+        currentPicture.setDescription(this.description.getText().toString());
+
+        // If title or description are invalid, do not allow saving
+        if (currentPicture.getTitle().trim().isEmpty() ||
+                currentPicture.getDescription().trim().isEmpty() ||
+                bitmap == null) {
+            Toast.makeText(getApplicationContext(), "Cannot save! Enter the title, description and image",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try { // Save Picture data into Database
+            if (currentPicture.getId() == null) { // New Picture
+                currentPicture.setPath(this.savePictureFile(this.bitmap, UUID.randomUUID()
+                        .toString() + ".jpg"));
+                pictureDao.insert(currentPicture);
+            } else { // Already Existent Picture
+                this.deletePictureFile(currentPicture.getPath()); // Deletes Previous Image
+                currentPicture.setPath(this.savePictureFile(this.bitmap, UUID.randomUUID()
+                        .toString() + ".jpg"));
+                pictureDao.update(currentPicture);
+            }
+
+            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            finish();
+        } catch (IOException e) {
+            Log.wtf("An error ocurred while saving the picture", e);
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void delete(View view) {
+        this.deletePictureFile(this.currentPicture.getPath());
+        pictureDao.delete(this.currentPicture);
+        finish();
+    }
+
+    public void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+//    --------------------------------------------------------
+//    util
+
+    private String savePictureFile(Bitmap imageToSave, String fileName) throws IOException {
+        try {
+            File file = new File(this.getApplicationContext().getFilesDir(), fileName);
+            if (file.exists()) {
+                file.delete();
+            }
+
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            return file.getPath();
+        } catch (IOException e) {
+            Log.wtf("Cannot save the picture", e);
+            throw e;
+        }
+    }
+
+    private void deletePictureFile(String filePath) {
+        File file = new File(filePath);
+        file.delete();
+    }
 
 }
